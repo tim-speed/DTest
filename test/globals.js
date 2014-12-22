ac142208c8a3498844b012153e1d109001eb31d3
Tim Speed <tspeed@compilr.com>
2014-12-22 17:04:24 -0400

    Fixed globals test

--- test/globals.js
+++ test/globals.js
@@ -1,3 +1,6 @@
+
+var randomVar = Math.random();
+
 /**
  * Represents a book.
  * @constructor
@@ -15,4 +18,39 @@ function Book(title, author) {
     } else {
         return null;
     }
+}
+
+/**
+ * Represents a book.
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function BookLearningNoTest(title, author) {
+    if (typeof title === 'string' && typeof author === 'string') {
+        return {
+            title: title,
+            author: author
+        };
+    } else {
+        return null;
+    }
+}
+
+/**
+ * Adds two numbers.
+ * @param {number} a - var a.
+ * @param {number} b - var b.
+ * @test 1 2 === 3
+ * @test -10 10 === 0
+ */
+function Add(a, b) {
+    return a + b;
+}
+
+/**
+ * Returns a random number generated at runtime
+ * @test typeof number
+ */
+function Random() {
+    return randomVar;
 }
