b0e593994f76523a187823ff7020ca6a91d2c633
Tim Speed <tspeed@compilr.com>
2014-12-25 09:29:55 -0400

    Support for static object traversing, next up prototypes and async

--- test/object.js
+++ test/object.js
@@ -1,5 +1,63 @@
 // Object based test test
 
 var theObj = {
-    
-}
+        /**
+         * Represents a book.
+         * @constructor
+         * @param {string} title - The title of the book.
+         * @param {string} author - The author of the book.
+         * @test "Jimmy's great adventure" "John Doe" typeof object
+         * @test undefined undefined !
+         */
+        Book: function Book(title, author) {
+            if (typeof title === 'string' && typeof author === 'string') {
+                return {
+                    title: title,
+                    author: author
+                };
+            } else {
+                return null;
+            }
+        },
+
+        /**
+         * Represents a book.
+         * @param {string} title - The title of the book.
+         * @param {string} author - The author of the book.
+         */
+        BookLearningNoTest: function BookLearningNoTest(title, author) {
+            if (typeof title === 'string' && typeof author === 'string') {
+                return {
+                    title: title,
+                    author: author
+                };
+            } else {
+                return null;
+            }
+        }
+    },
+    theOtherObj = {
+        /**
+         * Adds two numbers.
+         * @param {number} a - var a.
+         * @param {number} b - var b.
+         * @test 1 2 === 3
+         * @test -10 10 === 0
+         */
+        Add: function(a, b) {
+            return a + b;
+        },
+
+        subObj: {
+            /**
+             * Returns a random number generated at runtime
+             * @test typeof number
+             */
+            Random: function random() {
+                return Math.random();
+            }
+        }
+        
+    };
+
+
